c387785f8af9b488013d80b7f5096069e5154660
Luke Vance <lukevance90@gmail.com>
2016-02-18 19:01:17 -0700

    added basic form for new vision - for now on one page

--- js/controllers/newVision.controller.js
+++ js/controllers/newVision.controller.js
@@ -1,10 +1,20 @@
+'use strict';
+
 angular.module('vizdea')
   .controller('NewVizions', [NewVizions]);
-
 
 
 function NewVizions () {
   var vm = this;
   vm.title = 'Angular Template App';
   vm.subtitle = 'Welcome to Angular!';
+  vm.submit = submit;
+
+  function submit (who, why, what){
+    console.log(who);
+    console.log(why);
+    console.log(what);
+  }
+
+
 }
